b112954a6f885eae5c1d979d21fbfe08a674eee3
evanshango <evansonshango@gmail.com>
2021-12-13 10:02:44 +0300

    adding spring data jpa configuration. closes #1

---
+++ README.md
@@ -0,0 +1,4 @@
+[![CircleCI](https://circleci.com/gh/evanshango/mssc-ssm/tree/master.svg?style=svg)](https://circleci.com/gh/evanshango/mssc-ssm/tree/master)
+# MSSC Beer State Machine
+
+Spring Boot Microservice example
---
+++ src/main/java/com/codewithevans/msscssm/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.codewithevans.msscssm.repository;
+
+import com.codewithevans.msscssm.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
