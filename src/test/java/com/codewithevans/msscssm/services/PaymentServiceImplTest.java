11711f9172d00a62946ccc30073a75e0840866d0
evanshango <evansonshango@gmail.com>
2021-12-14 13:09:33 +0300

    adding state machine guards. closes #10

--- src/test/java/com/codewithevans/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/com/codewithevans/msscssm/services/PaymentServiceImplTest.java
@@ -1,11 +1,15 @@
 package com.codewithevans.msscssm.services;
 
 import com.codewithevans.msscssm.domain.Payment;
+import com.codewithevans.msscssm.domain.PaymentEvent;
+import com.codewithevans.msscssm.domain.PaymentState;
 import com.codewithevans.msscssm.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -39,5 +43,22 @@ class PaymentServiceImplTest {
 
         assertNotNull(preAuthedPayment);
         System.out.println(preAuthedPayment);
+    }
+
+    @Transactional
+    @RepeatedTest(10)
+    void testAuth(){
+        Payment savedPayment = paymentService.newPayment(payment);
+
+        StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+
+        if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH){
+            System.out.println("Payment is pre-authorized");
+            StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authPayment(savedPayment.getId());
+
+            System.out.println("Result of auth: " + authSM.getState().getId());
+        } else {
+            System.out.println("Payment failed pre-auth...");
+        }
     }
 }
