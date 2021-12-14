a751c8e1d1300d53b7a1aec9c4011846d25eb925
evanshango <evansonshango@gmail.com>
2021-12-14 09:18:08 +0300

    adding state machine initialization from the database. closes #5

---
+++ src/main/java/com/codewithevans/msscssm/exceptions/NotFoundException.java
@@ -0,0 +1,4 @@
+package com.codewithevans.msscssm.exceptions;
+
+public class NotFoundException extends RuntimeException{
+}
--- src/main/java/com/codewithevans/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/com/codewithevans/msscssm/services/PaymentServiceImpl.java
@@ -3,10 +3,12 @@ package com.codewithevans.msscssm.services;
 import com.codewithevans.msscssm.domain.Payment;
 import com.codewithevans.msscssm.domain.PaymentEvent;
 import com.codewithevans.msscssm.domain.PaymentState;
+import com.codewithevans.msscssm.exceptions.NotFoundException;
 import com.codewithevans.msscssm.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -24,16 +26,37 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authPayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId){
+        var payment = paymentRepository.findById(paymentId).orElseThrow(NotFoundException::new);
+
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(Long.toString(payment.getId()));
+
+        sm.stop();
+
+        sm.getStateMachineAccessor().doWithAllRegions(sma -> {
+            sma.resetStateMachine(new DefaultStateMachineContext<>(
+                    payment.getState(), null, null, null)
+            );
+        });
+
+        sm.start();
+
+        return sm;
     }
 }
