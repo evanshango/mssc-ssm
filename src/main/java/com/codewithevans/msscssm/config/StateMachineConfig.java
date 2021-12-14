2ffc7727d489a644020ee09bb6b9248cdb1a499f
evanshango <evansonshango@gmail.com>
2021-12-14 12:31:23 +0300

    adding state machine actions. closes #9

--- src/main/java/com/codewithevans/msscssm/config/StateMachineConfig.java
+++ src/main/java/com/codewithevans/msscssm/config/StateMachineConfig.java
@@ -2,8 +2,11 @@ package com.codewithevans.msscssm.config;
 
 import com.codewithevans.msscssm.domain.PaymentEvent;
 import com.codewithevans.msscssm.domain.PaymentState;
+import com.codewithevans.msscssm.services.PaymentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -13,6 +16,7 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
+import java.util.Random;
 
 @Slf4j
 @Configuration
@@ -28,6 +32,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
     @Override
     public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
         transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+                .action(preAuthAction())
                 .and().withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH)
                 .event(PaymentEvent.PRE_AUTH_APPROVED).and().withExternal().source(PaymentState.NEW)
                 .target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
@@ -35,7 +40,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
 
     @Override
     public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
-        StateMachineListenerAdapter<PaymentState, PaymentEvent> adapter = new StateMachineListenerAdapter<>(){
+        StateMachineListenerAdapter<PaymentState, PaymentEvent> adapter = new StateMachineListenerAdapter<>() {
             @Override
             public void stateChanged(State<PaymentState, PaymentEvent> from, State<PaymentState, PaymentEvent> to) {
                 log.info(String.format("stateChanged(from: %s, to: %s)", from, to));
@@ -43,5 +48,27 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
         };
 
         config.withConfiguration().listener(adapter);
+    }
+
+    public Action<PaymentState, PaymentEvent> preAuthAction() {
+        return context -> {
+            System.out.println("PreAuth was called");
+            if (new Random().nextInt(10) < 8) {
+                System.out.println("Approved");
+                context.getStateMachine().sendEvent(MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_APPROVED)
+                        .setHeader(
+                                PaymentServiceImpl.PAYMENT_HEADER_ID,
+                                context.getMessageHeader(PaymentServiceImpl.PAYMENT_HEADER_ID)
+                        ).build()
+                );
+            } else {
+                System.out.println("Declined! No Credit!!!");
+                context.getStateMachine().sendEvent(MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_DECLINED)
+                        .setHeader(
+                                PaymentServiceImpl.PAYMENT_HEADER_ID,
+                                context.getMessageHeader(PaymentServiceImpl.PAYMENT_HEADER_ID)
+                        ).build());
+            }
+        };
     }
 }
